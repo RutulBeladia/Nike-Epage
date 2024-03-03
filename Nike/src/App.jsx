9653d5a8e1e76cd3883f85815cd9b9155a9b1964
unknown <RutulBeladia>
2024-03-03 16:19:04 +0100

    first commit

---
+++ Nike/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Mainbody from "./components/Mainbody";
+import Navigation from "./components/Navigation";
+const App = () =>{
+    return ( 
+    <div>
+      <Navigation />
+      <Mainbody />
+    </div>
+    );
+};
+export  default App;
---
+++ Nike/src/components/Mainbody.jsx
@@ -0,0 +1,26 @@
+const Mainbody =()=>{
+    return <main className="hero container">
+        <div className="Content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+                YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <div className="btn-shop">
+                <button>Shop Now</button>
+                <button className="cat">Category</button>
+            </div>
+            <div className="shopping">
+                <p>Also Available On</p>
+                <div className="Brand-logos">
+                    <img src="./images/flipkart.png" alt="flipkart" />
+                    <img src="./images/amazon.png" alt="amazon" />    
+                </div> 
+            </div>
+        </div>
+        <div className="Image">
+            <img src="./images/shoe_image.png" alt="" />
+        </div>
+
+    </main>;
+};
+
+export default Mainbody;
